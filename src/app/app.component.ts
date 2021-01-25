47cbf6caad9380e52d388835eacfcef7ce0a42a9
Ch3shireDev <thesmilingcatofcheshire@gmail.com>
2021-01-25 21:34:10 +0100

    :rocket: First release

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 declare let MathJax: any;
+declare let math: any;
 
 @Component({
   selector: 'app-root',
@@ -16,19 +17,35 @@ export class AppComponent implements OnInit {
   X = ["-1", "0"]
   Y = ["-2", "0", "1"]
 
-  PX: number[];
-  PY: number[];
+  PX: any[];
+  PY: any[];
 
-  EX: number;
-  EY: number;
-  EX2: number;
-  EY2: number;
-  VarX: number;
-  VarY: number;
-  EXY:number;
-  CovXY:number;
-
+  EX: any;
+  EXs: any;
+  EY: any;
+  EYs: any;
+  EX2: any;
+  EX2s: any;
+  EY2: any;
+  EY2s: any;
+  VarX: any;
+  VarXs: any;
+  VarY: any;
+  VarYs: any;
+  EXY: any;
+  CovXY: any;
+  sigmax: any;
+  sigmaxs: any;
+  sigmay: any;
+  sigmays: any;
+  rhoXY: any;
+  rhoXYs: string;
   dataRows: string[][];
+
+  Z: any[];
+  PZ: any[];
+  W: any[];
+  PW: any[];
 
   constructor() { }
 
@@ -81,20 +98,22 @@ export class AppComponent implements OnInit {
     this.generate();
   }
 
-  public getMeanValue(X: string[], PX: number[]): number {
-    let sum = 0;
+  public getMeanValue(X: string[], PX: any[]): string[] {
+    let sum = [];
     for (let i = 0; i < X.length; i++) {
-      sum += eval(X[i]) * PX[i];
+      sum.push(`${X[i]} * ${PX[i]}`);
     }
-    return sum;
+    let x = sum.join("+");
+    return [x.replace(/\*/gi, '\\cdot'), math.simplify(x)];
   }
 
-  public getMeanValue2(X: string[], PX: number[]): number {
-    let sum = 0;
+  public getMeanValue2(X: string[], PX: any[]): any {
+    let sum = [];
     for (let i = 0; i < X.length; i++) {
-      sum += eval(X[i]) * eval(X[i]) * PX[i];
+      sum.push(`${X[i]}*${X[i]} * ${PX[i]}`);
     }
-    return sum;
+    let x = sum.join('+');
+    return [x.replace(/\*/gi, "\\cdot"), math.simplify(x)];
   }
 
   public generate() {
@@ -102,15 +121,120 @@ export class AppComponent implements OnInit {
 
     this.PX = this.generatePX();
     this.PY = this.generatePY();
-    this.EX = this.getMeanValue(this.X, this.PX);
-    this.EY = this.getMeanValue(this.Y, this.PY);
-    this.EX2 = this.getMeanValue2(this.X, this.PX);
-    this.EY2 = this.getMeanValue2(this.Y, this.PY);
-    this.VarX = this.EX2 - (this.EX) * this.EX;
-    this.VarY = this.EY2 - this.EY * this.EY;
+    [this.EXs, this.EX] = this.getMeanValue(this.X, this.PX);
+    [this.EYs, this.EY] = this.getMeanValue(this.Y, this.PY);
+    [this.EX2s, this.EX2] = this.getMeanValue2(this.X, this.PX);
+    [this.EY2s, this.EY2] = this.getMeanValue2(this.Y, this.PY);
+    this.VarXs = `${this.EX2} - (${this.EX})^2`;
+    this.VarX = math.simplify(this.VarXs);
+    this.sigmaxs = `\\sqrt{${this.VarX}}`;
+    this.sigmax = math.simplify(`sqrt(${this.VarX})`);
+    this.VarYs = `${this.EY2} - (${this.EY})^2`;
+    this.VarY = math.simplify(this.VarYs);
+    this.sigmays = `\\sqrt{${this.VarY}}`;
+    this.sigmay = math.simplify(`sqrt(${this.VarY})`);
 
+    this.EXY = this.getEXY();
+
+
+
+    this.CovXY = math.simplify(`${this.EXY}-(${this.EX})*(${this.EY})`);
+    this.rhoXY = math.simplify(`(${this.CovXY})/(sqrt(${this.VarX})*sqrt(${this.VarY}))`);
+    this.rhoXYs = `\\frac{${this.CovXY}}{\\sqrt{${this.VarX}}\\cdot\\sqrt{${this.VarY}}}`;
+
+    [this.Z, this.PZ] = this.generatePZ();
+    [this.W, this.PW] = this.generatePW();
+    this.renderMath();
+  }
+
+  public getEXY() {
+    let sum = [];
+    for (let i = 0; i < this.nx; i++) {
+      for (let j = 0; j < this.ny; j++) {
+        sum.push(`${this.X[i]}*${this.Y[j]}*${this.dataRows[j][i]}`)
+      }
+    }
+    return math.simplify(sum.join('+'));
+  }
+
+  public generatePZ(): any[][] {
+    let data = {};
+
+    for (let i = 0; i < this.nx; i++) {
+      for (let j = 0; j < this.ny; j++) {
+        let x = this.X[i];
+        let y = this.Y[j];
+        let z = math.simplify(`${x}+${y}`);
+        let p = this.dataRows[j][i];
+        if (z in data) {
+          data[z] += "+" + p;
+          data[z] = math.simplify(data[z]);
+        }
+        else {
+          data[z] = p;
+        }
+      }
+    }
+
+    let list = [];
+
+    for (var key in data) {
+      list.push({ key: key, value: data[key] });
+    }
+
+    list.sort((a, b) => a["key"] - b["key"]);
+
+    let Z = []
+    let PZ = []
+
+    for (var i = 0; i < list.length; i++) {
+      Z.push(list[i]["key"]);
+      PZ.push(list[i]["value"]);
+    }
+
+    return [Z, PZ];
+  }
+
+  public generatePW(): any[][] {
+    let data = {};
+
+    for (let i = 0; i < this.nx; i++) {
+      for (let j = 0; j < this.ny; j++) {
+        let x = this.X[i];
+        let y = this.Y[j];
+        let z = math.simplify(`${x}*${y}`);
+        let p = this.dataRows[j][i];
+        if (z in data) {
+          data[z] += "+" + p;
+          data[z] = math.simplify(data[z]);
+        }
+        else {
+          data[z] = p;
+        }
+      }
+    }
+
+    let list = [];
+
+    for (var key in data) {
+      list.push({ key: key, value: data[key] });
+    }
+
+    list.sort((a, b) => a["key"] - b["key"]);
+
+    let W = []
+    let PW = []
+
+    for (var i = 0; i < list.length; i++) {
+      W.push(list[i]["key"]);
+      PW.push(list[i]["value"]);
+    }
+
+    return [W, PW];
+  }
+
+  public renderMath() {
     setTimeout(() => {
-
       MathJax.Hub.Queue(["Typeset", MathJax.Hub]);
       this.isGenerated = true;
     }, 400);
@@ -119,24 +243,28 @@ export class AppComponent implements OnInit {
   public generatePX() {
     let arr = [];
     for (let i = 0; i < this.nx; i++) {
-      let sum = 0;
+      let sum = [];
       for (let j = 0; j < this.ny; j++) {
-        sum += eval(this.dataRows[j][i]);
+        sum.push(this.dataRows[j][i]);
       }
-      arr.push(sum);
+      arr.push(math.simplify(sum.join('+')));
     }
     return arr;
   }
   public generatePY() {
     let arr = [];
     for (let i = 0; i < this.ny; i++) {
-      let sum = 0;
+      let sum = [];
       for (let j = 0; j < this.nx; j++) {
-        sum += eval(this.dataRows[i][j]);
+        sum.push(this.dataRows[i][j]);
       }
-      arr.push(sum);
+      arr.push(math.simplify(sum.join("+")));
     }
     return arr;
+  }
+
+  public multiply(px, py): string {
+    return math.simplify(`${px}*${py}`);
   }
 
 
