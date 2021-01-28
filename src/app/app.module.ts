8e99ffc4d0957b3339aff4bc30e77d025328d942
Ch3shireDev <the.smiling.cat.of.cheshire@gmail.com>
2021-01-28 22:38:57 +0100

    :fox_face: Add PWA

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { MathJaxModule } from 'ngx-mathjax';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,8 @@ import { MathJaxModule } from 'ngx-mathjax';
       version: '2.7.5',
       config: 'TeX-AMS_HTML',
       hostname: 'cdnjs.cloudflare.com'
-    })
+    }),
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
